98b28306b0ab8f139e5086b338e4e885f5ef8210
Aytan-sudo <cantais.aymeric@gmail.com>
2026-09-14 16:40:58 +0200

    fix: conserver les caches des autres jeux

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== VERSION).map((key) => caches.delete(key))))
+      .then((keys) => Promise.all(keys.filter((key) => key.startsWith('laser-mirror-') && key !== VERSION).map((key) => caches.delete(key))))
       .then(() => self.clients.claim()),
   );
 });
